b60e1fab59b147086448068b2a7fcd96e3f6a514
donew <eastgloss0330@naver.com>
2024-04-07 14:01:33 +0900

    [itertools] 조합

--- snippets/itertools/조합_combinations.py
+++ snippets/itertools/조합_combinations.py
@@ -1,6 +1,10 @@
 from itertools import combinations as iter_combinations
 arr = [1, 2, 3, 4]
 all_combinations = []
+
+# 현재 인덱스를 의미하는 매개변수가 추가됨
+# 순서가 상관 없고 중복불가이기 때문에 현재 인덱스보다 작거나 같은 인덱스는 볼 필요가 없음
+# 재귀 돌릴 때 현재 인덱스 + 1 값을 넘긴다
 def combinations(n, current, index):
     if len(current) == n:
         all_combinations.append(current)
